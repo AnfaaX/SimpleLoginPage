7b5aa02ef795611b51523d62466fc5fd3029dbfe
AnfaaX <anfaa082003@gmail.com>
2020-08-09 20:17:35 +0700

    First Commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+++[ Simple LoginPage ]++
+
+Create by Andrian :
+
+-Facebook : Andrian
+-Telegram : @AnfaaMe
+
+# email : anfaa082003@gmail.com #
+
+Thank you
---
+++ js/stylesheet.js
@@ -0,0 +1,11 @@
+function sumbit(){
+   var mail = document.getElementById("mail").value;
+   var pass = document.getElementById("pass").value;
+   var pass2 = document.getElementById("pass2").value;
+   
+   if(mail == "anfaa082003@gmail.com "){
+      if(pass == "anfaa"){
+         window.alert("Login Sukses")
+      }
+   }
+}
